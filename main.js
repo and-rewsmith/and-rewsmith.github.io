4b0bf3c7a9eafdc0dd62ee52d31e316969a095be
als5ev <als5ev@virginia.edu>
2018-12-29 19:24:06 -0500

    responsive

---
+++ main.js
@@ -0,0 +1,7 @@
+var height = $("#entry-carousel").css('height');
+console.log("width:" + height + "px;");
+
+document.getElementById('carousel').setAttribute("style","width:" + height + "px;");
+
+document.getElementById('carousel-item').setAttribute("style","height:" + height + "px;");
+
