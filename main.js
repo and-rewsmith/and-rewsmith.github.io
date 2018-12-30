960f49cc6c0a124ae4e3612596ea7d023e12eba4
als5ev <als5ev@virginia.edu>
2018-12-29 20:32:57 -0500

    responive changes

--- main.js
+++
@@ -1,7 +0,0 @@
-var height = $("#entry-carousel").css('height');
-console.log("width:" + height + "px;");
-
-document.getElementById('carousel').setAttribute("style","width:" + height + "px;");
-
-document.getElementById('carousel-item').setAttribute("style","height:" + height + "px;");
-
